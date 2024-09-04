2178931ddbeeacae5986b3665cf834974502d418
Jashwanthi37 <gjashwanthi9@gmail.com>
2024-09-04 22:15:22 +0530

    tasks

---
+++ index.js
@@ -0,0 +1,29 @@
+//Task1
+console.log("Task1")
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+//Task2
+console.log("Task2")
+var num=5;
+for(var i=1;i<=10;i++){
+    table=num+"x"+i+"="+num*i;
+    console.log(table);
+}
+//task3
+console.log("Task3")
+add=0;
+for(var i=1;i<=10;i++){
+    add=add+i;
+}
+console.log(add);
+//task4
+console.log("Task4")
+for(var i=3;i<=30;i+=3){
+    console.log(i)
+}
+//task5{
+console.log("Task5")
+for(var i=1;i<=10;i++){
+    console.log(i*i);
+}
